feffc2ab4aca5ca6e2314f6f6afb5b9fe55c179d
kalpana-rcb <tomarekalpana@gmail.com>
2026-07-25 09:44:21 +0530

    Add files via upload

--- customer_details_insert.php
+++ customer_details_insert.php
@@ -1,21 +1,20 @@
 <?php
-include('database.php');
+include('admin/database.php');
 $Name=$_POST['cn'];
 $Address=$_POST['ca'];
 $City=$_POST['cc'];
 $Number=$_POST['cnor'];
 $Email=$_POST['em'];
-
+$password=$_POST["password"];
+$hint_qtn=$_POST["hint_qtn"];
+$hint_ans=$_POST["hint_ans"];
 $sql="insert into customer_details values(null,'$Name','$Address','$City','$Number','$Email')";
 mysqli_query($conn,$sql);
-
-$sql2="insert into login values('$Email','$Number','customer','Enter Ur Contact No','$Number','Active')";
-mysqli_query($conn,$sql2);
-
-
+$sql="insert into login values('$Email','$password','customer','active','$password','customer')";
+mysqli_query($conn,$sql);
 ?>
-<script>
-alert("inserted..");
-document.location="customer_details_view.php";
+<script type="text/javascript">
+alert("Customer Registered Successfully");
+document.location="login.php";
 </script>
 
